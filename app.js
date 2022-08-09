2010b2731b4423f8e3728b6cc2b59ef274d84159
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-08-09 16:25:52 +0800

    feat: add shorten function

--- app.js
+++ app.js
@@ -3,18 +3,44 @@ const app = express()
 const port = 3000
 require('./config/mongoose')
 const exphbs = require('express-handlebars')
+const Url = require('./models/url')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
- res.render('index')
+  res.render('index')
 })
 
-// app.post('/', (req, res ) => {
+app.post('/', (req, res) => {
+  const code = makeCode(5)
+  const url = req.body.url
+  Url.create({ code, url })
+    .then(() => res.render('done', { code }))
+    .catch(error => console.log(error))
+})
 
-// })
+app.get('/:code', (req, res) => {
+  const { code } = req.params
+  Url.findOne({ code })
+    .lean()
+    .then(website => res.redirect(`${website.url}`))
+    .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
   console.log(`app is listening on ${port}`)
 })
+
+function makeCode(number) {
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const collection = (lowerCaseLetters + upperCaseLetters + numbers).split('')
+  let code = ''
+  for (let i = 0; i < number; i++) {
+    code += collection[Math.floor(Math.random() * collection.length)]
+  }
+  return code
+}
--- models/url.js
+++ models/url.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const urlSchema = new Schema({
-  code: {type: string, require:true}
+  code: {type: String, require:true},
+  url: { type: String, require: true }
 })
 
 module.exports = mongoose.model('Url', urlSchema)
