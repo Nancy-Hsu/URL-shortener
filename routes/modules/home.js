7c2e0bc3abe4e661f7884cbdb3d54d002ac0bff0
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-08-17 17:21:52 +0800

    feat: modify error message

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,7 +10,7 @@ router.get('/:code', (req, res) => {
   const { code } = req.params
   Url.findOne({ code })
     .then(data =>
-      !data ? res.send('errorMsg: Can\'t found the URL') : res.redirect(`${data.url}`))
+      data ? res.redirect(`${data.url}`) : res.send('<h1>Sorry, this URL could not be found ...</h1>'))
     .catch(error => console.log(error))
 })
 
