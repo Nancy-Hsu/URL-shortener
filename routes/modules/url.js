d694ca19eded95ed1d96336a107684dea7999365
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-08-17 15:19:02 +0800

    refactor: url router code

--- routes/modules/url.js
+++ routes/modules/url.js
@@ -4,27 +4,19 @@ const Url = require('../../models/url')
 const makeCode = require('../../makeCode.js')
 
 router.post('/', (req, res) => {
-  const code = makeCode(5)
   const url = req.body.url
-  /// /check if code is repeated
-  Url.findOne({ code })
-    .lean()
-    .then(item => {
-      if (!item) return
-      makeCode(5)
-    })
-    .catch(error => console.log(error))
+  
   /// / check if url is repeated
   Url.findOne({ url })
-    .lean()
-    .then(website => {
-      if (!website) {
+    .then(data => {
+      if (!data) {
+        const code = makeCode(5)
         return Url.create({ code, url })
-          .then(() => res.render('done', { code }))
-          .catch(error => console.log(error))
       }
-      res.render('done', { code: website.code })
-    }).catch(error => console.log(error))
+      return data
+    })
+    .then(data => res.render('done', { code: data.code }))
+    .catch(error => console.log(error))
 })
 
 
